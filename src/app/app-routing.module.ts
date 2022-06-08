8a726653cf1372b6f47f8401a91d0361ac53219d
Famouse <famouse_ru@mail.ru>
2022-06-08 22:12:26 +0300

    Adding navigation to navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ScheduleComponent} from "./schedule/schedule.component";
+import {SoonComponent} from "./soon/soon.component";
 
 const routes: Routes = [
-  { path: '', component: ScheduleComponent}
+  {
+    path: '',
+    component: ScheduleComponent
+  },
+  {
+    path: 'soon',
+    component: SoonComponent
+  }
 ];
 
 @NgModule({
