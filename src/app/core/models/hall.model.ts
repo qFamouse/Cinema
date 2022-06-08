70976ae53a9b5b5f04351e69d97c224f3b8c794e
Famouse <famouse_ru@mail.ru>
2022-06-08 15:55:11 +0300

    Adding seances to hall model

--- src/app/core/models/hall.model.ts
+++ src/app/core/models/hall.model.ts
@@ -1,6 +1,9 @@
+import {Seance} from "./seance.model";
+
 export interface Hall {
   id: number,
   name: string,
   floor_count: number,
-  place_count: number
+  place_count: number,
+  seances: Seance[]
 }
