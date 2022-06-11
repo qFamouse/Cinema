959b5b4837a8108e6eec7913909aace8fa2811d4
Famouse <famouse_ru@mail.ru>
2022-06-11 15:47:05 +0300

    Try to Authorize when user load page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { UserService } from './core/services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor (private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.populate();
+  }
 
 }
