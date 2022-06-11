bbd29cd9729496da5924bdd264931571866da813
Famouse <famouse_ru@mail.ru>
2022-06-11 12:35:27 +0300

    Automatically putting bearer auth token to headers if user is logined

--- src/app/core/interceptors/http.token.interceptor.ts
+++ src/app/core/interceptors/http.token.interceptor.ts
@@ -1,11 +1,12 @@
 import { Injectable, Injector } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { JwtService } from '../services';
 
 
 @Injectable()
 export class HttpTokenInterceptor implements HttpInterceptor {
-  constructor(/* private jwtService: JwtService */) {}
+  constructor(private jwtService: JwtService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const headersConfig = {
@@ -13,11 +14,11 @@ export class HttpTokenInterceptor implements HttpInterceptor {
       'Accept': 'application/json'
     };
 
-    // const token = this.jwtService.getToken();
+    const token = this.jwtService.getToken();
 
-    // if (token) {
-    //   headersConfig['Authorization'] = `Token ${token}`;
-    // }
+    if (token) {
+      headersConfig['Authorization'] = `Bearer ${token}`;
+    }
 
     const request = req.clone({ setHeaders: headersConfig });
     return next.handle(request);
