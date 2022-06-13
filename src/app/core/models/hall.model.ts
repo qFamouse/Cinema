5b1bd99a21e4bcedd3fd1ed4ff9fef486ff6e3b4
Famouse <famouse_ru@mail.ru>
2022-06-13 23:00:50 +0300

    fix hall model

--- src/app/core/models/hall.model.ts
+++ src/app/core/models/hall.model.ts
@@ -3,7 +3,7 @@ import {Seance} from "./seance.model";
 export interface Hall {
   id: number,
   name: string,
-  floor_count: number,
-  place_count: number,
+  rows: number,
+  seats: number,
   seances: Seance[]
 }
